5e44a341d2270b6a1d62ba6d8fb778b98413f550
Garic152 <erictheiler15@gmail.com>
2023-05-06 12:00:14 +0200

    Finished FEN parser

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ Trying out some chess engine stuff
 #defining the piece integer representation
 e, P, N, B, R, Q, K, p, n, b, r, q, k, o = range(14)
 
+white, black = range(2)
+
 Castling = {'KC': 1, 'QC': 2, 'kc': 4, 'qc': 8}
 
 board = [
@@ -20,6 +22,7 @@ board = [
 ]
 
 char_ascii = '.PNBRQKpnbrqk'
+char_sides = 'wb'
 
 
 #from ascii to normal chars
@@ -30,11 +33,11 @@ start_position = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
 fen = 'r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1'
 
 
-#1 = white and 2 = black
-side_to_move = 1
+#0 = white and 1 = black
+side = white
 
 #castling rights in bit representation, where 15 is 1111, meaning both sides can castle both on the queen and king side
-can_castle = 15
+can_castle = 0
 
 
 def clear_board():
@@ -64,7 +67,14 @@ def print_board():
     print("\n    A B C D E F G H")
 
 
+def print_stats():
+    print("Side to move: " + char_sides[side])
+    print("Castling: " + str(bin(can_castle)[2:]).rjust(4, '0'))
+
+
 def load_fen(fen):
+    global side
+    global can_castle
     fen_position = 0
 
     # reset board
@@ -83,27 +93,43 @@ def load_fen(fen):
                     # set current board position to fen piece
                     board[position] = char_pieces[fen[fen_position]]
 
-                    # increase fen position
                     fen_position += 1
 
                 elif fen[fen_position] > '0' and fen[fen_position] < '9':
                     file += int(fen[fen_position]) - 1
 
-                    # increase fen position
                     fen_position += 1
 
-                elif fen[fen_position] == '/':
-                    # increase fen position
+                elif fen[fen_position] == '/' or fen[fen_position] == ' ':
                     fen_position += 1
-
             file += 1
+    
+    #go to side position
+    fen_position += 1
+    
+    #parse side to move
+    side = white if fen[fen_position] == 'w' else black
 
+    #go to castling position
+    fen_position += 2
 
-
+    while fen[fen_position] != ' ':
+        if fen[fen_position] == 'K':
+            can_castle |= Castling['KC']
+        elif fen[fen_position] == 'Q':
+            can_castle |= Castling['QC']
+        elif fen[fen_position] == 'k':
+            can_castle |= Castling['kc']
+        elif fen[fen_position] == 'q':
+            can_castle |= Castling['qc']
+        elif fen[fen_position] == '-':
+            break
+        fen_position += 1
 
 
 def main():
     load_fen(start_position)
+    print_stats()
     print_board()
 
 if __name__ == '__main__':
