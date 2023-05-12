61ea63e9aea08b943108ae5916a8df2682e8648a
eduardChalovski <85243017+eduardChalovski@users.noreply.github.com>
2023-05-12 18:31:12 +0200

    Update test_main.py
    several tests updated

--- test_main.py
+++ test_main.py
@@ -4,7 +4,7 @@
 
 import pytest
 import main
-
+from main import Moves
 #helpful features like parameterizing: allows to run multiple variations of the same test
 #@pytest.mark.parametrize('num1, num2, result', [
 #                                                   (7, 3, 10),
@@ -46,14 +46,191 @@ king_movement = [16, -16, 1, -1, 15, 17, -15, -17]
 
 white, black = range(2)
 
+
 #side=white
 can_castle=0
 
-#def setup_module(module):
-#    global side
-#    global can_castle 
-#    can_castle = main.can_castle
-#    side = main.side
+""" def setup_module(module):
+    global move2
+    move2 = Moves()
+ """
+
+start_board= [
+    r, n, b, q, k, b, n, r, o, o, o, o, o, o, o, o,
+    p, p, p, p, p, p, p, p, o, o, o, o, o, o, o, o,
+    e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+    e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+    e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+    e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+    P, P, P, P, P, P, P, P, o, o, o, o, o, o, o, o,
+    R, N, B, Q, K, B, N, R, o, o, o, o, o, o, o, o
+]
+promotion_board = [
+    e, e, b, q, k, b, n, r, o, o, o, o, o, o, o, o,
+    P, e, p, p, p, p, p, p, o, o, o, o, o, o, o, o,
+    e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+    e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+    e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+    e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+    e, P, P, P, P, P, P, P, o, o, o, o, o, o, o, o,
+    R, N, B, Q, K, B, N, R, o, o, o, o, o, o, o, o
+]
+
+capture_board= [
+    r, n, b, q, k, b, n, r, o, o, o, o, o, o, o, o,
+    p, p, p, p, p, p, p, p, o, o, o, o, o, o, o, o,
+    P, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+    e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+    e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+    e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+    e, P, P, P, P, P, P, P, o, o, o, o, o, o, o, o,
+    R, N, B, Q, K, B, N, R, o, o, o, o, o, o, o, o
+]
+
+castling_board =[
+    r, n, b, q, k, b, n, r, o, o, o, o, o, o, o, o,
+    p, p, p, p, p, p, p, p, o, o, o, o, o, o, o, o,
+    e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+    e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+    e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+    e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+    P, P, P, P, P, P, P, P, o, o, o, o, o, o, o, o,
+    R, e, e, e, K, e, e, R, o, o, o, o, o, o, o, o
+]
+
+
+@pytest.mark.parametrize('move,board_position, printed_result, board_result',
+                         [(main.set_move(main.square_representation.index('a2'),main.square_representation.index('a4'),0,0,1,0),
+                           start_board, "Moving a2 to a4\n", #pawn Move
+                           [r, n, b, q, k, b, n, r, o, o, o, o, o, o, o, o,
+                            p, p, p, p, p, p, p, p, o, o, o, o, o, o, o, o,
+                            e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+                            e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+                            P, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+                            e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+                            e, P, P, P, P, P, P, P, o, o, o, o, o, o, o, o,
+                            R, N, B, Q, K, B, N, R, o, o, o, o, o, o, o, o]),
+                           (main.set_move(main.square_representation.index("a7"),main.square_representation.index("a8"),Q,0,0,0),  #<- move
+                            promotion_board, 
+                            "Moving a7 to a8\n",          #auf der Konsole ausgegebenes String                                              #promotion into queen     
+                            [Q, e, b, q, k, b, n, r, o, o, o, o, o, o, o, o,
+                             e, e, p, p, p, p, p, p, o, o, o, o, o, o, o, o,
+                             e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+                             e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,        #<- wie das Board nach dem Zug aussehen soll
+                             e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+                             e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+                             e, P, P, P, P, P, P, P, o, o, o, o, o, o, o, o,
+                             R, N, B, Q, K, B, N, R, o, o, o, o, o, o, o, o])])
+def test_make_move(capsys, move, board_position, printed_result, board_result):  
+    main.board =board_position
+    main.make_move(move)
+    stdout, stderr = capsys.readouterr()
+    assert stdout == printed_result
+    assert main.board == board_result
+#,
+                           #  (main.set_move(main.square_representation.index("b7"),main.square_representation.index("a6"),0,1,0,0),
+                           #   capture_board, "Moving b7 to a6\n",           #capture of a pawn
+                           #   [r, n, b, q, k, b, n, r, o, o, o, o, o, o, o, o,
+                           # p, e, p, p, p, p, p, p, o, o, o, o, o, o, o, o,
+                           # p, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+                           # e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+                           # e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+                           # e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+                           # e, P, P, P, P, P, P, P, o, o, o, o, o, o, o, o,
+                           # R, N, B, Q, K, B, N, R, o, o, o, o, o, o, o, o]),
+                           # (main.set_move(main.square_representation.index("e1"),main.square_representation.index("c1"),0,0,0,1),
+                           # castling_board,"Moving e1 to c1\n",         #castling queen side
+                           # [r, n, b, q, k, b, n, r, o, o, o, o, o, o, o, o,
+                           #  p, p, p, p, p, p, p, p, o, o, o, o, o, o, o, o,
+                           #  e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+                           #  e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+                           #  e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+                           #  e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+                           #  P, P, P, P, P, P, P, P, o, o, o, o, o, o, o, o,
+                           #  e, e, K, R, e, e, e, R, o, o, o, o, o, o, o, o]),
+                           #  (main.set_move(main.square_representation.index("e1"),main.square_representation.index("g1"),0,0,0,1 ),
+                           # castling_board,"Moving e1 to g1\n",         #castling king side
+                           # [r, n, b, q, k, b, n, r, o, o, o, o, o, o, o, o,
+                           #  p, p, p, p, p, p, p, p, o, o, o, o, o, o, o, o,
+                           #  e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+                           #  e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+                           #  e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+                           #  e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
+                           #  P, P, P, P, P, P, P, P, o, o, o, o, o, o, o, o,
+                           #  R, e, e, e, e, R, K, e, o, o, o, o, o, o, o, o])])
+#def test_make_move(capsys, move, board_position, printed_result, board_result):  
+#    main.board =board_position
+#    main.make_move(move)
+#    stdout, stderr = capsys.readouterr()
+#    assert stdout == printed_result
+#    assert main.board == board_result
+
+@pytest.mark.parametrize('moves,index',[(Moves(),0), (Moves(),1), (Moves(), 2)])
+def test___getitem__(moves,index):
+    moves.add_move(0b1)
+    moves.add_move(0b10)
+    moves.add_move(0b11)
+    assert moves.moves[index] == moves[index]
+
+
+@pytest.mark.parametrize('moves, move',[(Moves(),0b0), (Moves(),0b1),(Moves(),0b10)])
+def test_add_move(moves,move):
+    mcount =moves.count
+    moves.add_move(move)
+    assert move == moves.moves[len(moves.moves)-1]
+    assert moves.count == mcount+1 
+
+def test___init__():
+    move1 = Moves()
+    assert move1.moves == []
+    assert move1.count == 0
+
+@pytest.mark.parametrize('move,result',[(0b000000000000000000000,0),
+                                        (0b100000000000000000000,1)])
+def test_get_move_castling(move,result):
+    assert main.get_move_castling(move) == result
+
+@pytest.mark.parametrize('move,result', [(0b00000000000000000000,0),
+                                         (0b10000000000000000000,1)])
+def test_get_move_pawn(move,result):
+    assert main.get_move_pawn(move) == result
+
+@pytest.mark.parametrize('move,result', [(0b0000000000000000000,0),
+                                         (0b1000000000000000000,1)])
+def test_get_move_capture(move,result):
+    assert main.get_move_capture(move) == result
+
+@pytest.mark.parametrize('move,result', [(0b000000000000000,0),
+                                         (0b111100000000000000,15)])
+def test_get_move_piece(move,result):
+    assert main.get_move_piece(move) == result
+
+@pytest.mark.parametrize('move,result',[(0b00000000,0),
+                                        (0b11111110000000,127)])
+def test_get_move_target(move,result):
+    assert main.get_move_target(move) == result
+
+@pytest.mark.parametrize('move,result',[(0b0,0),
+                                        (0b10,2),
+                                        (0b11,3),
+                                        (0b1111111,127)])
+def test_get_move_source(move,result):
+    assert main.get_move_source(move) == result
+
+@pytest.mark.parametrize('current,target,promotion_piece,capture,double_pawn,castling,result',[(1,0,0,0,0,0,0b1),
+                                                                                               (1,1,0,0,0,0,0b10000001),
+                                                                                               (1,1,N,0,0,0,0b1000000010000001),
+                                                                                               (1,1,B,0,0,0,0b1100000010000001),
+                                                                                               (1,1,R,0,0,0,0b10000000010000001),
+                                                                                               (1,1,Q,0,0,0,0b10100000010000001),
+                                                                                               (1,1,n,0,0,0,0b100000000010000001),
+                                                                                               (1,1,q,0,0,0,0b101100000010000001),
+                                                                                               (1,1,q,1,0,0,0b1101100000010000001),
+                                                                                               (1,1,q,1,1,0,0b11101100000010000001),
+                                                                                               (1,1,q,1,1,1,0b111101100000010000001),
+                                                                                               ])
+def test_set_move(current,target,promotion_piece,capture,double_pawn,castling,result):
+    assert bin(main.set_move(current,target,promotion_piece,capture,double_pawn,castling)) == bin(result)
 
 @pytest.mark.parametrize('fen,board_result,side_result,can_castle_result', [('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1',
                                                                             [r, n, b, q, k, b, n, r, o, o, o, o, o, o, o, o,
@@ -372,4 +549,5 @@ def test_clear_board():
                      e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o,
                      e, e, e, e, e, e, e, e, o, o, o, o, o, o, o, o]
  
+
 
